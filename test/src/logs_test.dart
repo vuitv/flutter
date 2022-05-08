8a5d7f9761ff9441e90f19bd478f6ccdd5e6a8a3
Vui TV <tangvuicntt@gmail.com>
2022-05-09 06:36:03 +0700

    responsive object

--- test/src/logs_test.dart
+++ test/src/logs_test.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:test/test.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:vuitv/src/utils/logs.dart';
 
 void main() {
