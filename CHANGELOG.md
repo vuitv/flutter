9ab84c5977eb4cdec72ffbd770789172587cb907
Vui TV <tangvuicntt@gmail.com>
2022-07-19 21:47:15 +0700

    check image url

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
 ## 0.0.1
 
 * Initial Open Source release.
+
+## 0.0.2
+
+* Initial Open Source release.
+
+## 0.0.3
+
+* Initial Open Source release.
--- lib/vuitv.dart
+++ lib/vuitv.dart
@@ -1,6 +1,7 @@
 library vuitv;
 
 export 'src/extension/json_serializer.dart';
+export 'src/extension/string.dart';
 export 'src/ui/layout/responsive.dart';
 export 'src/utils/colors.dart';
 export 'src/utils/logs.dart';
